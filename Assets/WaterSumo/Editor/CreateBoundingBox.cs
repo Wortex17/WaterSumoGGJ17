6983e1d5044452d8c808c238326c5bced9c04b69
Christian Fedrowitz <rdfoe@me.com>
2017-01-20 23:59:48 +0100

    Editor Script for creating and modifying the Arena

---
+++ Assets/WaterSumo/Editor/CreateBoundingBox.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum EShape {Circle = 0, Rectangle = 1}
+
+
+	public class CreateBoundingBox : EditorWindow {
+
+	private EShape shapeSelection;
+	private GUILayoutOption option;
+
+
+	[MenuItem ("Window/Create Bounding Box")]
+	static void MenuButton()
+	{
+		CreateBoundingBox boundingBox = (CreateBoundingBox)EditorWindow.GetWindow (typeof (CreateBoundingBox));
+		boundingBox.Show();
+
+	}
+
+	void OnGUI()
+	{
+		shapeSelection = (EShape) EditorGUILayout.EnumPopup("Bounding Box to create:", shapeSelection);
+
+		if (GUILayout.Button("Create"))
+			CreateTheBoundingBox(shapeSelection);
+
+	}
+
+	void CreateTheBoundingBox(EShape _selectedShape)
+	{
+		switch((int)_selectedShape)
+		{
+		case 0:
+			GameObject circle = new GameObject ("Arena");
+			circle.AddComponent<CapsuleCollider> ().radius = 0.5f;
+			circle.GetComponent<CapsuleCollider> ().height = 5.0f;
+			circle.AddComponent<ArenaBehaviour>();
+			Debug.Log ("Circle");
+			break;
+		case 1:
+			GameObject rectangle = new GameObject ("Arena");
+			rectangle.AddComponent<BoxCollider> ().size = new Vector3 (1, 0.1f, 1);
+			rectangle.AddComponent<ArenaBehaviour>();
+			Debug.Log ("Rectangle");
+			break;
+		}
+	}
+}
---
+++ Assets/WaterSumo/Scripts/ArenaBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ArenaBehaviour : MonoBehaviour {
+
+	[SerializeField, Range(0, 100)]
+	private float CapsuleColliderRadius = 0.5f;
+	[SerializeField]
+	private float CapsuleColliderHeight = 5.0f;
+
+	[SerializeField, Range(0, 100)]
+	private float BoxColliderSize = 1.0f;
+
+	private CapsuleCollider capsuleColl;
+	private BoxCollider boxColl;
+
+	private bool GOCheck;
+
+	// Use this for initialization
+	void Start () {
+
+		if (GetComponent<CapsuleCollider> () != null)
+		{
+			capsuleColl = GetComponent<CapsuleCollider> ();
+			GOCheck = false;
+		}
+
+		if (GetComponent<BoxCollider> () != null)
+		{
+			GOCheck = true;
+			boxColl = GetComponent<BoxCollider> ();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ModifyColl ();
+
+	}
+
+	void OnValidate()
+	{
+		
+	}
+
+	void ModifyColl()
+	{
+		if (GOCheck)
+		{
+			boxColl.size = new Vector3 (BoxColliderSize, 0.1f, BoxColliderSize);
+		} 
+		else
+		{
+			capsuleColl.radius = CapsuleColliderRadius;
+			capsuleColl.height = CapsuleColliderHeight;
+		}
+	}
+}
