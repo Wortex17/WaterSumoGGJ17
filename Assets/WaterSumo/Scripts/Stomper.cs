5e01a4c0e52db5fbb83450f395363a07bac50b94
Christian Fedrowitz <rdfoe@me.com>
2017-01-21 00:23:15 +0100

    Stomper

---
+++ Assets/WaterSumo/Scripts/Stomper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stomper : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject Wave;
+
+	private float stompingTimer = 0;
+
+	[SerializeField]
+	private float stompingInterval = 1;
+
+	[SerializeField]
+	private float timeMultiplicator;
+
+	void Start () {
+
+		// TODO: Get the wave reference by code or drag an drop in inspector?!
+
+	}
+	
+	void Update () {
+
+		stompingTimer += Time.deltaTime * timeMultiplicator;
+
+		if (stompingTimer >= stompingInterval)
+			STOMPIT ();
+
+		Debug.Log ("Timer" + stompingTimer);
+		
+	}
+
+	void STOMPIT()
+	{
+		// Create waves?!?!
+		Instantiate(Wave);
+
+		// TODO: Add some Coordinates for instantiating maybe?
+		Debug.Log("BinDrin");
+		// ResetTimer
+		stompingTimer = 0;
+	}
+}
