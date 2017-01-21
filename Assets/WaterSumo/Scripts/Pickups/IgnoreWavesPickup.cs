d0f848b5727be015ea3244f61eefd06aabcea8fd
DESKTOP-OUEBSEP\Baldu <balduin.brandenburger@web.de>
2017-01-21 18:44:26 +0100

    Pickup sound

---
+++ Assets/WaterSumo/Scripts/Pickups/IgnoreWavesPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaterSumo
+{
+	public class IgnoreWavesPickup : TimeBasedPickup
+	{
+		override public void ActivatePickUp(PlayerController _playerController)
+		{
+			base.ActivatePickUp(_playerController);
+
+			_playerController.PlayerWaveAffected.IsPushedByWaves = false;
+		}
+
+		override public void DeactivatePickUp(PlayerController _playerController)
+		{
+			base.DeactivatePickUp(_playerController);
+
+			_playerController.PlayerWaveAffected.IsPushedByWaves = true;
+
+		}
+	}
+}
