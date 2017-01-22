5bb8001e8f20c52688426ed0abd1c5468bde3d72
Christian Fedrowitz <rdfoe@me.com>
2017-01-22 03:04:24 +0100

    Pause Menu

---
+++ Assets/WaterSumo/Scripts/PauseToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseToggle : MonoBehaviour {
+
+	private bool stopTheTiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiime = false;
+	[SerializeField]
+	private GameObject Overlay;
+
+	// Use this for initialization
+	void Awake () {
+
+		Overlay.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButton("PlayerControllerStart"))
+			{
+				stopTheTiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiime = !stopTheTiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiime;
+			}
+
+		if (stopTheTiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiime)
+		{
+			Time.timeScale = 0;
+			Overlay.SetActive (true);
+		} 
+		else
+		{
+			Time.timeScale = 1;
+			Overlay.SetActive (false);
+		}
+
+	}
+}
